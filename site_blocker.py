d2501082a89cdb989a0b1e9c290f6d125765d959
Professional <hate4fake@gmail.com>
2020-11-08 02:28:45 +0700

    Reformate code

--- site_blocker.py
+++ site_blocker.py
@@ -2,7 +2,7 @@ import time
 from datetime import datetime as dt
 
 sites_to_block = [
-    'www.facebook.com',  'facebook.com',
+    'www.facebook.com', 'facebook.com',
     'www.youtube.com', 'youtube.com',
     'www.gmail.com', 'gmail.com'
 ]
@@ -12,15 +12,17 @@ Window_host = r"C:\Windows\System32\drivers\etc\hosts"
 default_hoster = Window_host
 redirect = "127.0.0.1"
 
-def block_websites(start_hour , end_hour):
+
+def block_websites(start_hour, end_hour):
     while True:
-        if dt(dt.now().year, dt.now().month, dt.now().day,start_hour)< dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,end_hour):
+        if dt(dt.now().year, dt.now().month, dt.now().day, start_hour) < dt.now() < dt(dt.now().year, dt.now().month,
+                                                                                       dt.now().day, end_hour):
             print("Do the work ....")
             with open(default_hoster, 'r+') as hostfile:
                 hosts = hostfile.read()
-                for site in  sites_to_block:
+                for site in sites_to_block:
                     if site not in hosts:
-                       hostfile.write(redirect+' '+site+'\n')
+                        hostfile.write(redirect + ' ' + site + '\n')
         else:
             with open(default_hoster, 'r+') as hostfile:
                 hosts = hostfile.readlines()
@@ -31,6 +33,7 @@ def block_websites(start_hour , end_hour):
                 hostfile.truncate()
             print('Good Time')
         time.sleep(3)
+
 
 if __name__ == '__main__':
     block_websites(9, 21)
